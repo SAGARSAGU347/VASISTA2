7f3c41c4bd399b3e135694bc645f5aa25ad7cfe5
SAGARSAGU347 <162672930+SAGARSAGU347@users.noreply.github.com>
2024-11-14 19:27:56 +0530

    Add files via upload

---
+++ src/landing_page/home/Sponsors.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+
+function Sponsors() {
+  return (
+    <>
+    
+    <section class="clients-section mb-5">
+        <div class="container text-center">
+            <h5 style={{color: "#e73725" } }>A COMMUNITY OF PARTNERS</h5>
+            <h2 style={{fontSize:"4em"}}>Our Clients</h2>
+            
+        </div>
+        <div class="custom-line"></div>
+        </section>
+
+        <div class="sponsors"> 
+          <img src='/images/acer.png' alt='dd'></img><img src='/images/amazon_pay.png ' alt='dd'></img><img src='/images/airtel_xtreme.png' alt='dd'></img><img src='/images/amd.png' alt='dd'></img><img src='/images/aorus.png' alt='dd'></img><img src='/images/rog.png' alt='dd'></img><img src='/images/cellbell.png'  alt='dd'></img><img src='/images/rooter.png' alt='dd'></img>
+        </div>
+
+        
+
+        
+    
+    
+ 
+        </>
+  )
+}
+
+export default Sponsors;
